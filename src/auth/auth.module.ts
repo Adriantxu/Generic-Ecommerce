1ac50c684eeeb6e1c664629967cc4363fa50d2bb
Oriol Linan <oriol.linan@epitech.eu>
2023-05-01 19:18:18 +0200

    removed test

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   controllers: [AuthController],
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { databaseProviders } from './database.provider';
+import { User } from './models/user.model';
+import { Categories } from './models/categories.model';
+import { ProductCategories } from './models/product_categories.model';
+import { ShoppingCart } from './models/shopping_cart.model';
+import { Product } from './models/product.model';
 
 @Module({
-    providers: [...databaseProviders],
-    exports: [...databaseProviders],
+  providers: [...databaseProviders],
+  exports: [
+    ...databaseProviders,
+  ],
 })
 export class DatabaseModule {}
